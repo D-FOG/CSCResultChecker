d0ff42f19494262a5a967c07ca182712b5a2c172
Favour Ogbonda <97465239+D-FOG@users.noreply.github.com>
2023-12-25 03:31:16 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
           <img src={HomeLogo} alt="Uniport Logo" />
         </div>
         <div className="inner_home_bottom">
-          <h3>Welcome To The Official Computer Science Student Result Checker</h3>
+          <h3>Welcome To The Official Computer Science Student Result Management System</h3>
           <div className="bottom_buttons">
             <NavLink to="/check_result"><button>Check Result</button></NavLink>
             <NavLink to="/register"><button>Register</button></NavLink>
